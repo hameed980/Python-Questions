535e2cd853a300b7aedefcb75d0e53d3a8ec0017
abdul hameed <abdulhameedak247@gmail.com>
2024-12-24 12:06:17 +0500

    added solution qu.11 to 20 and solutions files

--- file_handling/solutions.py
+++ file_handling/solutions.py
@@ -45,10 +45,83 @@
 #     print(len(content))
 
 # QUESTION10 (SOLUTION):
-with open('hello.txt','r') as file:
-    lines = file.readlines()
-with open("reversed_data.txt",'w') as file:
-    for line in reversed(lines):
-       file.write(line)
-       
+# with open('hello.txt','r') as file:
+#     lines = file.readlines()
+# with open("reversed_data.txt",'w') as file:
+#     for line in reversed(lines):
+#        file.write(line)
 
+# QUESTION 11 (SOLUTION):
+# from collections import Counter
+# with open("hello.txt",'r') as f:
+#     data = f.read()
+#     frequency = Counter(data)
+# print(dict(frequency))
+
+# QUESTION 12 (SOLUTION):
+# with open('hello.txt','r') as f:
+#     data = f.read()
+#     new_data = data.replace("hello",'hi')
+# with open("hello.txt",'w') as f:
+#     f.write(new_data)
+# print(new_data)
+
+# QUESTION 13(SOLUTION):
+# with open('hello.txt','r') as file1, open('copy.txt','r') as file2,open('merged.txt','w') as outfile:
+#     outfile.write(file1.read() + '\n' + file2.read())
+    
+# QUESTION 14(SOLUTION):
+# with open("hello.txt",'r') as f:
+#     line = f.readlines()
+# with open("hello.txt",'w') as f:
+#     for i in line:
+#        if i.strip():
+#            f.write(i)
+
+# QUESTION 15(SOLUTION):
+import csv
+# data = [["Name","Age"],
+#         ["abdul",18],
+#         ['hameed',19]
+# ]
+# with open('hello.txt','w',newline='') as f:
+#    writer =  csv.writer(f)
+#    writer.writerows(data)
+
+# QUESTION 16(SOLUTION):
+# with open('hello.txt','r') as f:
+#     data = csv.reader(f)
+#     for i in data:
+#         print(i)
+
+# QUESTION 17(SOLUTION):
+# with open('copy.txt','r') as f:
+#     data = f.readlines()
+# data.sort()
+# with open("copy.txt",'w') as f:
+#     f.writelines(data)
+
+# QUESTION 18(SOLUTION):
+# with open('hello.txt','r') as f:
+#     data = f.read()
+# unique_words = set(data.split())
+# print(len(unique_words))
+    
+# QUESTION 19(SOLUTION):
+# num1 = 10
+# num2 = 0
+# try:
+#     result = num1/num2
+#     print(result)
+# except ZeroDivisionError as e:
+#     with open('error.log','a') as f:
+#         f.write(f'Error {e} \n')
+
+# QUESTION 20(SOLUTION):
+# import json
+# data = {"name":"abdul","age":18}
+# with open ('data.json','w') as f:
+#     json.dump(data,f)
+# with open ('data.json','r') as f:
+#     result = json.load(f)
+# print(result)
---
+++ file_handling/tempCodeRunnerFile.py
@@ -0,0 +1,1 @@
+with open ('data.json','r') as f:
